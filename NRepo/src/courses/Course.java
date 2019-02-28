9bc54e51cb1e6d39d4b6a006e99f1fc9359f6024
Nirex0 <Nirex.0@gmail.com>
2019-03-01 01:13:29 +0330

    Create: Course class

---
+++ NRepo/src/courses/Course.java
@@ -0,0 +1,27 @@
+package courses;
+
+public class Course {
+    public Course(String cName, int unit) {
+        name = cName;
+        unitCount = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String in) {
+        name = in;
+    }
+
+    public int getUnitCount() {
+        return unitCount;
+    }
+
+    public void setUnitCount(int in) {
+        unitCount = in;
+    }
+
+    private String name;
+    private int unitCount;
+}
