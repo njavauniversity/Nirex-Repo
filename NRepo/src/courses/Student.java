a1dfc1537637f3c1d5c74acb94632c8fd8ddb6f1
Nirex0 <Nirex.0@gmail.com>
2019-03-01 01:13:34 +0330

    Create: Student class

---
+++ NRepo/src/courses/Student.java
@@ -0,0 +1,69 @@
+package courses;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Student {
+    public Student(String fName, String lName) {
+        courses = new ArrayList<>();
+        grades = new HashMap<>();
+        firstName = fName;
+        lastName = lName;
+    }
+
+    public void PrintAll() {
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Courses: ");
+
+        int iter = 0;
+        for (Course tmp : courses) {
+            iter++;
+            System.out.println("Course Number " + iter + "'s Name: " + tmp.getName());
+            System.out.println("Course Number " + iter + "'s Unit count: " + tmp.getUnitCount());
+        }
+
+        for (Course tmp : courses) {
+            if (grades.containsKey(tmp)) {
+                System.out.println("Grade for \"" + tmp.getName() + "\" course: " + grades.get(tmp));
+            }
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String in) {
+        firstName = in;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String in) {
+        lastName = in;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<Course> in) {
+        courses = in;
+    }
+
+    public HashMap<Course, Integer> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(HashMap<Course, Integer> in) {
+        grades = in;
+    }
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Course> courses;
+    private HashMap<Course, Integer> grades;
+}
