e27a8d6a7a91c968c65e342c061d753f051139dc
Nirex0 <Nirex.0@gmail.com>
2019-05-16 01:06:39 +0430

    Create: CRUD Test

---
+++ NRepo/src/crudtest/Test.java
@@ -0,0 +1,15 @@
+package crudtest;
+
+import java.sql.SQLException;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        Student std0 = new Student(1,"Aryan","Mousavi","Tehran");
+        std0.Insert();
+
+        Student std1 = new Student(2,"Danial","Jerban","Karaj");
+        std0.Update(std1);
+
+        std0.Delete();
+    }
+}
